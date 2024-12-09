3472e92c3048f0e2d788ef2a0984b38a8df66700
hieuhhhhhhh <160786237+hieuhhhhhhh@users.noreply.github.com>
2024-12-08 22:50:39 -0500

    updated profile frag

--- app/src/main/java/com/example/finalproject/ProfileFrag.kt
+++ app/src/main/java/com/example/finalproject/ProfileFrag.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.google.firebase.auth.FirebaseAuth
 
@@ -14,6 +15,19 @@ class ProfileFrag : Fragment(R.layout.fragment_profile) {
 
         // Get reference to the logout button
         val logoutButton = view.findViewById<Button>(R.id.button5)
+        // Get reference to the TextView where the email will be displayed
+        val emailTextView = view.findViewById<TextView>(R.id.textView4)
+
+        // Get the current user
+        val currentUser = FirebaseAuth.getInstance().currentUser
+
+        // Check if the user is logged in and display the email
+        currentUser?.let {
+            val email = it.email
+            emailTextView.text = "Email Address: ${email ?: "No email found"}"
+        } ?: run {
+            emailTextView.text = "No user logged in"
+        }
 
         // Set an OnClickListener on the logout button
         logoutButton.setOnClickListener {
